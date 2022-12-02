05a59225db876a7b1463b310e7c95077b6db8ca2
cecilieh <cecilieh@kth.se>
2022-12-02 17:48:52 +0100

    added standard error as function of N

--- assignment3/monte_carlo.py
+++ assignment3/monte_carlo.py
@@ -6,24 +6,30 @@ from tqdm import tqdm
 from numba import njit
 
 # typing
-from typing import Callable, Tuple
+from typing import Tuple
 from numpy import ndarray
 
 # %%
+@njit
 def metropolis(
-    P: Callable, 
-    f: Callable,
     seed: int = 1,
     x_0: float = 0,
     delta: float = 10,
     n_0: int = 100,
-    n: int = 10_000,
+    n: int = 100_000,
 ) -> Tuple[float, float]:
-    random_state = rnd.RandomState(seed=seed)
+
+    def f(x: ndarray) -> ndarray:
+        return x
+
+    def P(x: float) -> float:
+        return 0 if x < 0 else np.exp(-x)
+    
+    rnd.seed(seed)
     n_steps = n + n_0
     x = np.zeros(n_steps)
-    d = random_state.uniform(-delta, delta, size=n_steps)
-    r = random_state.uniform(0, 1, size=n_steps)
+    d = rnd.uniform(-delta, delta, size=n_steps)
+    r = rnd.uniform(0, 1, size=n_steps)
 
     x[0] = x_0
     for i in range(n_steps-1):
@@ -39,19 +45,21 @@ def metropolis(
     standard_error = std / np.sqrt(n)
     return mean, standard_error
 
-def f(x: ndarray) -> ndarray:
-    return x
-
-def P(x: float) -> float:
-    return 0 if x < 0 else np.exp(-x)
-
 # %% Plot values for different values of delta between 0.01 and 10
 delta = np.logspace(-2, 1, 100)
 means = np.zeros_like(delta)
 std_errors = np.zeros_like(delta)
 
 for i, d in tqdm(list(enumerate(delta))):
-    means[i], std_errors[i] = metropolis(P, f, delta=d, n_0=10, n=100_000)
+    means[i], std_errors[i] = metropolis(delta=d, n_0=10, n=100_000)
+
+# %% Plot values for different values of n between 100 and 100_000
+n = np.logspace(2, 5, 100, dtype=np.int32)
+means_n = np.zeros_like(n, dtype=np.float64)
+std_errors_n = np.zeros_like(n, dtype=np.float32)
+
+for i, n_i in tqdm(list(enumerate(n))):
+    means_n[i], std_errors_n[i] = metropolis(n_0=10, n=n_i)
 
 # %%
 plt.title("Metropolis algorithm, n_0=10, n=100_000", fontsize=16)
@@ -62,6 +70,17 @@ plt.ylabel("Error", fontsize=14)
 plt.legend()
 plt.grid()
 plt.savefig("metropolis.png")
+plt.show()
+
+# %%
+plt.title("Metropolis algorithm, function of N", fontsize=16)
+plt.semilogx(n, std_errors_n, 'x-', label='standard error')
+plt.semilogx(n, np.abs(1-means_n), 'x-', label='difference to the exact answer')
+plt.xlabel("N", fontsize=14)
+plt.ylabel("Error", fontsize=14)
+plt.legend()
+plt.grid()
+plt.savefig("metropolis_std_error.png")
 plt.show()
 
 # %%
