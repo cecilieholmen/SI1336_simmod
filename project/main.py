fixed calculation and plot of energies

project/main.py
@@ -66,14 +66,16 @@ class SolarSystem:
 
 class Observables:
 
-    def __init__(self):
+    def __init__(self, solar_system: SolarSystem) -> None:
+        self.names = [body.name for body in solar_system.bodies]  # names
+        self.colors = [body.color for body in solar_system.bodies]  # colors
         self.time = []  # list to store time
         self.positions = []  # list to store positions
         self.velocities = []  # list to store velocities
+        self.kinetic_energy = []  # list to store kinetic energies
+        self.potential_energy = []  # list to store potential energies
+        self.total_energy = []  # list to store total energies
 
-        self.kinetic_energy = []  # list to store kinetic energy
-        self.potential_energy = []  # list to store potential energy
-        self.total_energy = []  # list to store total energy
 
 # %% Integrators
 class BaseIntegrator(ABC):
@@ -88,30 +90,39 @@ class BaseIntegrator(ABC):
 
     def integrate(self, solar_system, obs) -> None:
         """Perform a single integration step"""
+        # calculate energies before integrating
+
         self.timestep(solar_system)
 
-        # Append observables to their lists
+        # Append time observables to their lists
         solar_system.time += self.dt
         obs.time.append(solar_system.time)
 
         positions = []
         velocities = []
-        kinetic_energy = []
-        potential_energy = []
-        total_energy = []
+        potential_energies = []
+        kinetic_energies = []
+        total_energies = []
         for body in solar_system.bodies:
+            kinetic = 0.5 * body.mass * np.sum(np.square(body.velocity))
+            potential = 0
+            for other in solar_system.bodies:
+                if body == other:
+                    continue
+                r = other.position - body.position
+                potential = potential - G * body.mass * other.mass / np.sqrt(np.sum(np.square(r)))
+
             positions.append(body.position)
             velocities.append(body.velocity)
-            if body.name != 'Sun':
-                kinetic_energy.append(0.5 * body.mass * np.sum(np.square(body.velocity)))
-                potential_energy.append(-G * body.mass * solar_system.bodies[0].mass / np.sqrt(np.sum(np.square(body.position - solar_system.bodies[0].position))))
-                total_energy.append(kinetic_energy[-1] + potential_energy[-1])
-            
+            kinetic_energies.append(kinetic)
+            potential_energies.append(potential)
+            total_energies.append(kinetic + potential)
+
         obs.positions.append(positions)
         obs.velocities.append(velocities)
-        obs.kinetic_energy.append(kinetic_energy)
-        obs.potential_energy.append(potential_energy)
-        obs.total_energy.append(total_energy)
+        obs.kinetic_energy.append(kinetic_energies)
+        obs.potential_energy.append(potential_energies)
+        obs.total_energy.append(total_energies)
 
 
 class EulerCromerIntegrator(BaseIntegrator):
@@ -168,7 +179,7 @@ class Simulation:
     # Plot the animation of the solar system using matplotlib and the observables
     def plot_simulation(self) -> None:
         fig = plt.figure()
-        size = 64983964400
+        size = 1e10
         ax = plt.axes(xlim=(-size, size), ylim=(-size, size))
         ax.set_aspect("equal")
         ax.grid()
@@ -193,28 +204,68 @@ class Simulation:
             return lines
 
         anim = animation.FuncAnimation(
-            fig, animate, init_func=init, frames=self.steps, interval=20, blit=True, 
+            fig, animate, init_func=init, frames=self.steps, interval=1, blit=True, 
         )
         plt.legend()
         plt.show()
 
+# %% Plot energies for all bodies
 
+def plot_kinetic_energy(obs: Observables) -> None:
+    plt.figure()
+    plt.title("Kinetic energy")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Kinetic energy [J]")
+    for i, name in enumerate(obs.names):
+        plt.plot(obs.time, [l[i] for l in obs.kinetic_energy], label=name, color=obs.colors[i])
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+def plot_potential_energy(obs: Observables) -> None:
+    plt.figure()
+    plt.title("Potential energy")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Potential energy [J]")
+    for i, name in enumerate(obs.names):
+        plt.plot(obs.time, [l[i] for l in obs.potential_energy], label=name, color=obs.colors[i])
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+def plot_total_energy(obs: Observables) -> None:
+    plt.figure()
+    plt.title("Total energy")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Total energy [J]")
+    for i, name in enumerate(obs.names):
+        plt.plot(obs.time, [l[i] for l in obs.total_energy], label=name, color=obs.colors[i])
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+# %% Main
 # Run simulation
 start_time = 0
 
-# Source data: https://ssd.jpl.nasa.gov/horizons/app.html#/ (mass, position, velocity) (82459928.500000000 = A.D. 2022-Dec-15 00:00:00.0000 TDB)
+# Source data: https://nssdc.gsfc.nasa.gov/planetary/factsheet/ (mass, position, velocity)
 bodies = [
     Body(
         name=data["name"],
         color=data["color"],
-        mass=data["mass"] * 10 ** 24,
-        position=np.array(data["position"], dtype=np.float64) * 10 ** 7,
+        mass=data["mass"],
+        position=np.array(data["position"], dtype=np.float64),
         velocity=np.array(data["velocity"], dtype=np.float64),
     ) for data in json.load(open('/Users/cecilie/Desktop/Skrivbord – Cecilies MacBook Air/Universitet/tredje/simmod/project/planet_data.json'))
 ]
 
 sys = SolarSystem(bodies, start_time)
-integrator = EulerCromerIntegrator(dt=100_000_000)
-obs = Observables()
-sim = Simulation(sys, integrator, number_of_steps_per_frame=1, steps=100_000_000_000_000, obs=obs)
+integrator = EulerCromerIntegrator(dt=100_000)
+obs = Observables(sys)
+sim = Simulation(sys, integrator, number_of_steps_per_frame=1, steps=100_000_000, obs=obs)
 sim.plot_simulation()
+
+# Plot energies
+plot_kinetic_energy(obs)
+plot_potential_energy(obs)
+plot_total_energy(obs)